a07f6163ae0e63980e44f70e7f9949bb2d6f48bb
LHM <2585283373@qq.com>
2018-12-04 23:23:47 +0800

    add user.js interface

--- server/interface/users.js
+++ server/interface/users.js
@@ -5,3 +5,197 @@ import User from '../dbs/models/users'
 import Passport from './utils/passport'
 import Email from '../dbs/config'
 import axios from './utils/axios'
+
+let router = new Router({
+  prefix: '/users'
+})
+
+let Store = new Redis().client // 获取redis的客户端
+
+// 注册
+router.post('/signup', async (ctx) => {
+  const {
+    username,
+    password,
+    email,
+    code
+  } = ctx.request.body;
+
+  if (code) {
+    // 从redis中获取，在nodemail发验证码时会在redis中存储
+    const saveCode = await Store.hget(`nodemail:${username}`, 'code')
+    const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+    if (code === saveCode) {
+      if (new Date().getTime - saveExpire > 0) {
+        ctx.body = {
+          code: -1,
+          msg: '验证码已过期，请重新尝试'
+        }
+        return false
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: '请填写正确的验证码'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '请填写验证码'
+    }
+  }
+  let user = await User.find({
+    username
+  })
+  if (user.length) {
+    ctx.body = {
+      code: -1,
+      msg: '已被注册'
+    }
+    return
+  }
+  let nuser = await User.create({
+    username,
+    password,
+    email
+  })
+  if (nuser) {
+    // 写库成功进行登录动作
+    let res = await axios.post('/users/singnin', {
+      username,
+      password
+    })
+    if (res.data && res.data.code === 0) {
+      ctx.body = {
+        code: 0,
+        msg: '注册成功',
+        user: res.data.user
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: 'error'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '注册失败'
+    }
+  }
+})
+
+// 登录
+router.post('/signin', async (ctx, next) => {
+  // local策略返回的信息
+  return Passport.authenticate('local', function (err, user, info, status) {
+    if (err) {
+      ctx.body = {
+        code: -1,
+        msg: err
+      }
+    } else {
+      if (user) {
+        ctx.body = {
+          code: 0,
+          msg: '登录成功',
+          user
+        }
+        // 做登录动作
+        return ctx.login(user)
+      } else {
+        ctx.body = {
+          code: 1,
+          msg: info
+        }
+      }
+    }
+  })(ctx, next)
+})
+
+// 验证码
+router.post('/verify', async (ctx, next) => {
+  let username = ctx.request.body.username
+  const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+  // 限制频繁刷接口
+  if (saveExpire && new Date().getTime() - saveExpire < 0) {
+    ctx.body = {
+      code: -1,
+      msg: '验证请求过于频繁，1分钟内1次'
+    }
+    return false
+  }
+  // 发送对象
+  let transporter = nodeMailer.createTransport({
+    host: Email.smtp.host,
+    port: 587,
+    secure: false, // true监听465端口
+    auth: { // 验证
+      user: Email.smtp.user,
+      pass: Email.smtp.pass
+    }
+  })
+  // 接受信息
+  let ko = {
+    code: Email.smtp.code(),
+    expire: Email.smtp.expire(),
+    email: ctx.request.body.email,
+    user: ctx.request.body.username
+  }
+  // 邮件中显示什么内容
+  let mailOption = {
+    from: `认证邮件<${Email.smtp.user}>`,
+    to: ko.email, // 给谁发,接收方
+    subject: '高防美团全栈实战注册码', // 主题
+    html: `你在课程中注册，你的邀请码是${ko.code}` // 显示内容
+  }
+  // 发送
+  await transporter.sendMail(mailOption, (error, info) => {
+    if (error) {
+      return console.log('error')
+    } else {
+      // 存储
+      Store.hmset(`nodemail:${ko.user}`,'code', ko.code, 'expire', ko.expire, 'email', ko.email)
+    }
+  })
+  ctx.body = {
+    code: 0,
+    msg: '验证码已发送，可能会有延时，有效期1分钟'
+  }
+})
+
+// 退出
+router.get('/exit', async (ctx, next) => {
+  // 执行退出的动作
+  await ctx.login()
+  // 检查现在是不是登录状态
+  if (!ctx.isAuthenticated()) {
+    ctx.body = {
+      code: 0
+    }
+  } else {
+    ctx.body = {
+      code: -1
+    }
+  }
+})
+
+// 获取用户名
+router.get('/getUser', async (ctx) => {
+  // 检查现在是不是登录状态
+  if (ctx.isAuthenticated()) {
+    const {username, email} = ctx.session.password.user
+    ctx.body = {
+      user: username,
+      email
+    }
+  } else {
+    ctx.body = {
+      user: '',
+      email: ''
+    }
+  }
+})
+
+export default router
---
+++ server/interface/utils/passport.js
@@ -0,0 +1,34 @@
+import passport from 'koa-passport'
+ // 策略
+import LocalStrategy from 'passport-local'
+import UserModel from '../../dbs/models/user'
+
+// 调local策略
+passport.use(new LocalStrategy(async function (username, password, done) {
+  let where = {
+    username
+  };
+  let result = await UserModel.findOne(where)
+  if (result != null) {
+    if (result.password === password) {
+      return done(null, result)
+    } else {
+      return done(null, false, '密码错误')
+    }
+  } else {
+    return done(null, false, '用户不存在')
+  }
+}))
+
+// 每次请求的时候会从session中读取用户对象，查询用户验证登录成功后会把用户的数据存储到session中
+// 用户每次进来的时候都通过session去验证，需要做序列化的动作
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
+
+// 反序列化
+passport.deserializeUser(function (user, done) {
+  return done(null, user)
+})
+
+export default passport
