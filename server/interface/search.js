bf6de257e8b0e4bbf392094ceeee9355e96bb118
LHM <2585283373@qq.com>
2019-01-06 21:46:44 +0800

    产品详情页

--- server/interface/search.js
+++ server/interface/search.js
@@ -3,7 +3,9 @@ import axios from './utils/axios'
 import Poi from '../dbs/models/poi'
 // import sign from './utils/sign'
 let sign = 'abcd'
-let router = new Router({prefix: '/search'})
+let router = new Router({
+  prefix: '/search'
+})
 
 router.get('/top', async (ctx) => {
   try {
@@ -112,7 +114,9 @@ router.get('/resultsByKeywords', async (ctx) => {
     count: 1,
     pois: [{
       type: '文物古迹',
-      photos: [{url: 'http://img1.imgtn.bdimg.com/it/u=1447993537,2047087692&fm=26&gp=0.jpg'}],
+      photos: [{
+        url: 'https://p0.meituan.net/deal/201210/09/1_1009110059.jpg@220w_125h_1e_1c'
+      }],
       name: '故宫',
       biz_ext: {
         rating: 95,
@@ -124,7 +128,9 @@ router.get('/resultsByKeywords', async (ctx) => {
 
     }, {
       type: '文物古迹2',
-      photos: [{url: 'https://p0.meituan.net/travel/83544ca4b38bbe0f7644982c3528defd117921.jpg@220w_125h_1e_1c'}],
+      photos: [{
+        url: 'https://p0.meituan.net/travel/83544ca4b38bbe0f7644982c3528defd117921.jpg@220w_125h_1e_1c'
+      }],
       name: '故宫2',
       biz_ext: {
         rating: 96,
@@ -133,40 +139,77 @@ router.get('/resultsByKeywords', async (ctx) => {
       tag: '景酒2 背景合并就看见菲菲姐诶分解放军风机房IE发',
       tel: '189895222222',
       location: '116.46, 39.92',
-
     }]
   }
 })
 
-// router.get('/products', async (ctx) => {
-//   let keyword = ctx.query.keyword || '旅游'
-//   let city = ctx.query.city || '北京'
-//   let {
-//     status,
-//     data: {
-//       product,
-//       more
-//     }
-//   } = await axios.get('http://cp-tools.cn/search/products', {
-//     params: {
-//       keyword,
-//       city,
-//       sign
-//     }
-//   })
-//   if (status === 200) {
-//     ctx.body = {
-//       product,
-//       more: ctx.isAuthenticated() ? more: [],
-//       login: ctx.isAuthenticated()
-//     }
-//   }else{
-//     ctx.body = {
-//       product: {},
-//       more: ctx.isAuthenticated() ? more: [],
-//       login: ctx.isAuthenticated()
-//     }
-//   }
-// })
+router.get('/products', async (ctx) => {
+  // let keyword = ctx.query.keyword || '旅游'
+  // let city = ctx.query.city || '北京'
+  // let {
+  //   status,
+  //   data: {
+  //     product,
+  //     more
+  //   }
+  // } = await axios.get('http://cp-tools.cn/search/products', {
+  //   params: {
+  //     keyword,
+  //     city,
+  //     sign
+  //   }
+  // })
+  // if (status === 200) {
+  //   ctx.body = {
+  //     product,
+  //     more: ctx.isAuthenticated() ? more: [],
+  //     login: ctx.isAuthenticated()
+  //   }
+  // }else{
+  //   ctx.body = {
+  //     product: {},
+  //     more: ctx.isAuthenticated() ? more: [],
+  //     login: ctx.isAuthenticated()
+  //   }
+  // }
+
+  ctx.body = {
+    product: {
+      name: '海碗居北京菜',
+      biz_ext: {
+        rating: 96,
+        cost: 95
+      },
+      address: '地址地址',
+      photos: [{
+        url: 'http://p1.meituan.net/poi/0545af7cd341622a60d2f7b6cbd312df24576.jpg@380w_214h_1e_1c',
+        title: '图片'
+      } , {
+        url: 'https://p1.meituan.net/travel/57918668162cda545a37aa5d472e62e853125.jpg@220w_125h_1e_1c',
+        title: '图片'
+      }, {
+        url: 'https://p0.meituan.net/wedding/923aa59d88a9a938765def152782611b115496.jpg@220w_125h_1e_1c',
+        title: '图片'
+      }],
+      tel: '189895222222',
+      location: '116.46, 39.92',
+    },
+    more: ctx.isAuthenticated() ?
+      [{
+        name: '干炸丸子',
+        photos: [{
+          url: 'http://p1.meituan.net/poirichness/menu_9226479_683602361.jpg@130w_130h_1e_1c',
+          title: '图片'
+        }],
+        biz_ext: {
+          ticket_ordering: '',
+          rating: 96,
+          cost: 95
+        },
+      }]
+      : [],
+    login: ctx.isAuthenticated()
+  }
+})
 
 export default router
